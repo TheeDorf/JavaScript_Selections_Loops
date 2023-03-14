e662e02b18c2787ca87996d8a056642093a0fa12
AJ Dorfman <ajdorfman91@gmail.com>
2023-03-14 14:08:02 -0700

    finish section 4

--- app.js
+++ app.js
@@ -26,13 +26,84 @@ for (let i = 1; i <= 100; i++){
        
     }
 }
+// ex 3
 
 let i = 1
 while (i <= 100) {
     if (i % 2 !== 0){
         console.log(i); 
     }
-    i++ 
+    i++;    
+}
+
+i=1
+while (i <= 100) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('FIZZBUZZ')
+        
+    }
+    else if (i %3 === 0) {
+        console.log('FIZZ')
+        
+    }
+    else if (i % 5 ===0){ 
+    console.log('BUZZ')}
+        
+    i++;
     
 }
+i=1
+
+
+
+
+do {
+    if (i % 2 !== 0) {
+      console.log(i);
+    }
+    i++;
+  } while (i <= 100);
+  
+
+
+
+
+  i=1
+do {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('FIZZBUZZ')
+        
+    }
+    else if (i %3 === 0) {
+        console.log('FIZZ')
+        
+    }
+    else if (i % 5 ===0){ 
+    console.log('BUZZ')}
+
+    else {
+    
+    }
+        
+    i++;
+    
+} while (i<=100);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
