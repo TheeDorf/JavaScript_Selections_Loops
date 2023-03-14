774f91c512230dab0c25f811491765ef30126ff1
AJ Dorfman <ajdorfman91@gmail.com>
2023-03-14 14:44:13 -0700

    finish exercise

--- app.js
+++ app.js
@@ -112,7 +112,30 @@ for (let i = 0; i <= n; i++) {
 console.log(result)
 
 
+let fizzDivisor = Math.round(Math.random() * (10 - 1) + 1); 
+// creates a random number between 1 and 10
+let buzzDivisor = Math.round(Math.random() * (10 - 1) + 1); 
+// creates a random number between 1 and 10
+let start = Math.round(Math.random() * (10 - 1) + 1); 
+// creates a random number between 1 and 10
+let end = Math.round(Math.random() * (1000 - 100) + 100); 
+// creates a random number between 100 and 1000
 
+console.log(fizzDivisor)
+console.log(buzzDivisor)
 
+for (i = start; i <= end; i++){
+    
 
+if (i % fizzDivisor === 0 && i % buzzDivisor === 0) {
+    console.log('FIZZBUZZ')
+    
+}
+else if (i % fizzDivisor === 0) {
+    console.log('FIZZ')
+    
+}
+else if (i % buzzDivisor===0){ 
+console.log('BUZZ')}
 
+}
